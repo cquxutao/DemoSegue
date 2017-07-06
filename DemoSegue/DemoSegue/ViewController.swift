21ab06499a44c7dc5ebc91bb144b4cd32c209a94
徐涛 <xutao@TennyXu.local>
2017-07-06 19:15:44 +0800

    Thursday, 6 July 2017 at 19:15:30

---
+++ DemoSegue/DemoSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSegue
+//
+//  Created by 徐涛 on 06/07/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
